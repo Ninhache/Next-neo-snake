a0c62690906121e07da3ca342d2b50acecba0ee2
Ninhache <mr.carambar27@gmail.com>
2024-06-02 21:08:36 +0200

    feat: add a little bit of content, we need to work on the API Before doing something else

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,48 @@
+import { siteConfig } from "@/config/nav-link";
+import {
+  PageActions,
+  PageHeader,
+  PageHeaderDescription,
+  PageHeaderHeading,
+} from "./_components/PageHeader";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
+import Link from "next/link";
+import { Icons } from "./_components/icons";
+
 export default function Home() {
-  return <main>Home page</main>;
+  return (
+    <div className="container relative">
+      <PageHeader>
+        <PageHeaderHeading className="hidden md:block">
+          Discover the Snake Revolution
+        </PageHeaderHeading>
+        <PageHeaderHeading className="md:hidden">
+          Discover the Snake Revolution
+        </PageHeaderHeading>
+        <PageHeaderDescription>
+          One of the most iconic games redesigned in speedrun mode
+          <br />
+          Play and create your own maps for endless fun and challenge
+        </PageHeaderDescription>
+        <PageActions>
+          <Link href="/docs" className={cn(buttonVariants(), "w-24")}>
+            <Icons.play className="scale-75" />
+            <span className="px-1">Play</span>
+          </Link>
+          <Link
+            target="_blank"
+            rel="noreferrer"
+            href={siteConfig.links.github}
+            className={cn(buttonVariants({ variant: "outline" }), "w-24 gap-2")}
+          >
+            <Icons.brush className="scale-150" />
+            <span>Create</span>
+          </Link>
+        </PageActions>
+      </PageHeader>
+
+      <section>home page :D</section>
+    </div>
+  );
 }
