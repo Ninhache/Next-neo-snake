2bd0360938569b375d3739bedee13ac86e838cfe
Ninhache <mr.carambar27@gmail.com>
2024-06-02 20:57:33 +0200

    feat: remove useless things (I will redo them since the api gonna change too)

--- app/_@types/scenario/Online.ts
+++
@@ -1,7 +0,0 @@
-import { BaseScenarioData, Scenario } from "./Scenario";
-
-export class OnlineScenario extends Scenario<BaseScenarioData> {
-  constructor(serializedMap: string) {
-    super(serializedMap);
-  }
-}
