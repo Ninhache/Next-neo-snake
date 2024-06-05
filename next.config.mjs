35747dadb3d4a09afc614ce3ecb54f71b6875019
Ninhache <mr.carambar27@gmail.com>
2024-06-05 15:19:21 +0200

    fix: rename file according to the doc (why it was not an mjs..)

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+import withBundleAnalyzer from "@next/bundle-analyzer";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+const bundleAnalyzerConfig = withBundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
+});
+
+// Wrap MDX and Next.js config with each other
+export default bundleAnalyzerConfig(nextConfig);
