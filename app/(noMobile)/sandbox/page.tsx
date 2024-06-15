111bfedb2fd89140f4e3abd86cbfb5fd999d2b4e
Ninhache <mr.carambar27@gmail.com>
2024-06-15 18:54:11 +0200

    feat: add threejs

--- app/(noMobile)/sandbox/page.tsx
+++ app/(noMobile)/sandbox/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { PageHeader, PageHeaderHeading } from "@/app/_components/PageHeader";
 import { RefObject, useEffect, useRef } from "react";
 import * as THREE from "three";
 
@@ -21,40 +22,43 @@ export class ThreeRenderer {
 
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setSize(width, height);
-    this.renderer.shadowMap.enabled = true;
+    // this.renderer.shadowMap.enabled = true;
     ref.current?.appendChild(this.renderer.domElement);
 
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    cube.castShadow = true;
-    cube.receiveShadow = true;
+    // const geometry = new THREE.BoxGeometry();
+    // const material =
+    const cube = new THREE.Mesh(
+      new THREE.BoxGeometry(),
+      // new THREE.MeshPhongMaterial({ color: 0x00ff00 })
+      new THREE.MeshBasicMaterial({ color: 0x00ff00 }),
+    );
+
+    // cube.receiveShadow = true;
     cube.name = "cube";
     this.scene.add(cube);
 
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-    directionalLight.position.set(5, 0, 5);
-    directionalLight.castShadow = true;
-    directionalLight.shadow.mapSize.width = 1024;
-    directionalLight.shadow.mapSize.height = 1024;
-    directionalLight.shadow.camera.near = 0.5;
-    directionalLight.shadow.camera.far = 500;
-    directionalLight.shadow.camera.left = -50;
-    directionalLight.shadow.camera.right = 50;
-    directionalLight.shadow.camera.top = 50;
-    directionalLight.shadow.camera.bottom = -50;
-    this.scene.add(directionalLight);
+    // const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    // directionalLight.position.set(5, 0, 5);
+    // directionalLight.castShadow = true;
+    // directionalLight.shadow.mapSize.width = 1024;
+    // directionalLight.shadow.mapSize.height = 1024;
+    // directionalLight.shadow.camera.near = 0.5;
+    // directionalLight.shadow.camera.far = 500;
+    // directionalLight.shadow.camera.left = -50;
+    // directionalLight.shadow.camera.right = 50;
+    // directionalLight.shadow.camera.top = 50;
+    // directionalLight.shadow.camera.bottom = -50;
+    // this.scene.add(directionalLight);
 
-    const ambientLight = new THREE.AmbientLight(0x707070);
-    this.scene.add(ambientLight);
+    // const ambientLight = new THREE.AmbientLight(0x707070);
+    // this.scene.add(ambientLight);
   }
 
   public loop(): void {
     const renderScene = () => {
-      // Access the cube via the scene graph
       const cube = this.scene.getObjectByName("cube");
       if (cube) {
-        cube.rotation.x += 0.01;
+        cube.rotation.x += 0.02;
         cube.rotation.y += 0.01;
       }
 
@@ -66,7 +70,7 @@ export class ThreeRenderer {
   }
 }
 
-export default function Home() {
+export default function Sandbox() {
   const ref = useRef<HTMLDivElement>(null);
 
   const width = 800;
@@ -76,11 +80,16 @@ export default function Home() {
     if (typeof window !== "undefined") {
       const renderer = new ThreeRenderer({ width, height, ref });
       renderer.loop();
+
+      ref.current?.scrollIntoView({ behavior: "smooth", block: "center" });
     }
   }, []);
 
   return (
     <main className="container relative">
+      <PageHeader>
+        <PageHeaderHeading className="block">Sandbox</PageHeaderHeading>
+      </PageHeader>
       <section
         ref={ref}
         className="z-30 flex flex-wrap justify-center gap-x-2 gap-y-4"
